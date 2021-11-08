122f772d3bde4b6ff88fe6aca6391644261a3162
Concheetah <32611069+Concheetah@users.noreply.github.com>
2021-11-08 21:09:59 +0100

    blogposts fixen

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,42 +1,40 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import {graphql} from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-
-const BlogPage = ({data}) => {
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
 
 
+const BlogIndex = ({ data }) => {
+  const { edges: posts } = data.allMdx
   return (
-    <Layout pageTitle="My Blog Posts">
+    <Layout pageTitle="My posts">
       <ul>
-      {
-        data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
-          </article>
-        ))
-      }
+        {posts.map(({ node: post }) => (
+          <li key={post.id}>
+            <Link to={post.slug}>
+              <h2>{post.frontmatter.title}</h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query {
-    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-      nodes {
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
+export const pageQuery = graphql`
+  query blogIndex {
+    allMdx {
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            title
+          }
+          
+            slug
+          
         }
-        id
-        body
       }
     }
   }
 `
-export default BlogPage
+export default BlogIndex
