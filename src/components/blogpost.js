7f6f402da4f24992cb607ba3b93957a6b4a30488
Concheetah <32611069+Concheetah@users.noreply.github.com>
2021-11-28 12:29:17 +0100

    nieuwe huisstijl

--- src/components/blogpost.js
+++ src/components/blogpost.js
@@ -7,6 +7,7 @@ import Layout from "./layout"
 const shortcodes = { Link } // Provide common components here
 export default function PageTemplate({ data: { mdx } }) {
   return (
+
     <Layout>
       <h1>{mdx.frontmatter.title}</h1>
       <MDXProvider components={shortcodes}>
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@ import {container,
   navLinkItem,
   navLinkText,
   siteTitle,
+  headContainer,
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
@@ -19,22 +20,27 @@ const Layout = ({ pageTitle, children }) => {
     }
   `)
     return (
+     <>
+     <div className={headContainer}>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+       <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+       <nav>
+         <ul className={navLinks}>
+           <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+           <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+           <li className={navLinkItem}><Link to="/blog" className={navLinkText}> Blog</Link>
+           </li>
+         </ul>
+       </nav>
+       </div>
     <div className={container}>
-     <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-          <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-          <li className={navLinkItem}><Link to="/blog" className={navLinkText}> Blog</Link>
-          </li>
-        </ul>
-      </nav>
+     
       <main>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
+    </>
   )
 }
 
