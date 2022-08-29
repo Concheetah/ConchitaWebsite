79df91716aee6a281f4da4e3a7809c24c867bc1d
Conchita <conchitamartin94@gmail.com>
2022-08-29 09:23:43 +0200

    layout, triangle

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/styles.css'
+
+export default function Contact({children}){
+    return(
+        <div className='contact'>
+            {children}
+        </div>)
+
+}
